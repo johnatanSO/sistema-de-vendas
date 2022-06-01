4de0e127b0f28778ef88764909dbc8aa4c81692b
“Johnatan <jonystrit@gmail.com>
2022-06-01 17:01:11 -0300

    add back

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: 'https://vendergas-desafio-production.up.railway.app',
 });
