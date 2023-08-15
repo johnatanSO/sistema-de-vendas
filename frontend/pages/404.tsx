ed90a179b2174133597073c2ff01a3f288accaa5
johnatanSO <jonystrit@gmail.com>
2023-08-15 14:21:05 -0300

    :sparkles: [FEAT] - Adicionando verificação de sessão nas páginas através do getServerSideProps.

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,7 +1,25 @@
+import { usersService } from '../src/services/usersService'
+
 export default function NotFoundPage() {
   return (
     <>
       <h1>Página não encontrada</h1>
     </>
   )
+}
+
+export const getServerSideProps = async (context: any) => {
+  const hasSession = await usersService.getSession(context)
+  if (!hasSession) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/login',
+      },
+      props: {},
+    }
+  }
+  return {
+    props: {},
+  }
 }
--- frontend/pages/clientes.tsx
+++ frontend/pages/clientes.tsx
@@ -1,4 +1,5 @@
 import { Clients } from '../src/screens/Clients'
+import { usersService } from '../src/services/usersService'
 import { PageProps } from './_app'
 
 export default function ClientsPage({ setTitle }: PageProps) {
@@ -8,4 +9,20 @@ export default function ClientsPage({ setTitle }: PageProps) {
       <Clients />
     </>
   )
+}
+
+export const getServerSideProps = async (context: any) => {
+  const hasSession = await usersService.getSession(context)
+  if (!hasSession) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/login',
+      },
+      props: {},
+    }
+  }
+  return {
+    props: {},
+  }
 }
