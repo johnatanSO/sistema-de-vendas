47b0efac603b333e4ba511488059cb7ff441832b
Johnatan <devsantosjohn@gmail.com>
2024-08-26 18:42:24 -0300

    :sparkles: [FEAT] - Retornando status error correto para unauthorized.

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
-  secretToken: 'b266fd9110e2a1a83398105a8d6cec43',
-  secretRefreshToken: 'ef1a849e85eb65ddab3fc07f05f22da3',
+  secretToken: '55be645bbd881c643aa7ad20be45377c',
+  secretRefreshToken: 'b36239f6b88341e51418583109f73cc5',
   expiresInToken: '55m',
   expiresInRefreshToken: '30d',
   expiresRefreshTokenDays: 30,
--- backend/src/middlewares/ensureAuthenticated.ts
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -3,6 +3,7 @@ import { verify } from 'jsonwebtoken'
 import { AppError } from '../errors/AppError'
 import { UsersRepository } from '../repositories/Users/UsersRepository'
 import * as dotenv from 'dotenv'
+import auth from '../config/auth'
 dotenv.config()
 
 export async function ensureAuthenticated(
@@ -14,16 +15,20 @@ export async function ensureAuthenticated(
   if (!authHeader) throw new AppError('Token não informado', 401)
   const [, token] = authHeader.split(' ')
 
-  const { sub: userId } = verify(token, process.env.SECRET)
+  try {
+    const { sub: userId } = verify(token, auth.secretToken)
 
-  const usersRepository = new UsersRepository()
-  const user = await usersRepository.findById(userId.toString())
-
-  if (!user) throw new AppError('Usuário inválido', 401)
-
-  req.user = {
-    userId: userId.toString(),
+    const usersRepository = new UsersRepository()
+    const user = await usersRepository.findById(userId.toString())
+  
+    if (!user) throw new AppError('Usuário inválido', 401)
+  
+    req.user = {
+      userId: userId.toString(),
+    }
+    next()
+  } catch (err) {
+    console.log('ERROR', err)
+    throw new AppError(`Token inválido - ${err.message}`, 401)
   }
-
-  next()
 }
